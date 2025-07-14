4a2e3d9c30bfec025b8d63c00f4f40195eb4bf69
guiguiriosbit <guiguiriosbit@gmail.com>
2025-07-14 12:06:40 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-@@ .. @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Dashboard } from './pages/Dashboard';
 import { RegistrosPage } from './pages/RegistrosPage';
 import { SalidasPage } from './pages/SalidasPage';
-+import { VentasPage } from './pages/VentasPage';
+import { VentasPage } from './pages/VentasPage';
 
 function App() {
-@@ .. @@
-         <Routes>
-           <Route path="/" element={<Dashboard />} />
-           <Route path="/registros" element={<RegistrosPage />} />
-           <Route path="/salidas" element={<SalidasPage />} />
-+          <Route path="/ventas" element={<VentasPage />} />
-         </Routes>
-       </Router>
-     </div>
-   );
- }
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/registros" element={<RegistrosPage />} />
+          <Route path="/salidas" element={<SalidasPage />} />
+          <Route path="/ventas" element={<VentasPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
